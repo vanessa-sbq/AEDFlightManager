7ab9508a5dc281613a48e86d51bcb867285868f8
vanessa-sbq <up202207919@up.pt>
2023-12-26 12:48:32 +0000

    Added definitions and empty implementations of operations

--- src/FlightManager.cpp
+++ src/FlightManager.cpp
@@ -1,1 +1,56 @@
 #include "FlightManager.h"
+
+void FlightManager::parseData(){
+    // ToDo
+}
+
+
+// 1
+void FlightManager::printGlobalNumAirports(){
+    // ToDo
+}
+void FlightManager::printGlobalNumFlights(){
+    // ToDo
+}
+
+
+// 2
+void FlightManager::printNumFlightsOutOfAirport(){
+    // ToDo
+}
+
+
+// 3
+void FlightManager::printNumFlightsCityAirline(){
+    // ToDo
+}
+
+
+// 4
+void FlightManager::printNumCountriesFromAirport(){
+    // ToDo
+}
+
+
+// 5
+void FlightManager::printNumReachableDests(){
+    // ToDo
+}
+
+
+// 6
+void FlightManager::printMaxTrip(){
+    // ToDo
+}
+
+
+// 7
+void FlightManager::printTopKAirport(){
+    // ToDo
+}
+
+
+// 8
+void FlightManager::printEssentialAirports(){
+    // ToDo
+}
--- src/FlightManager.h
+++ src/FlightManager.h
@@ -4,6 +4,27 @@
 /**@brief Class that does operations on the data.*/
 class FlightManager{
 public:
+    // Network statistics
+    void parseData();
+    // 1
+    void printGlobalNumAirports();
+    void printGlobalNumFlights();
+    // 2
+    void printNumFlightsOutOfAirport();
+    // 3
+    void printNumFlightsCityAirline();
+    // 4
+    void printNumCountriesFromAirport();
+    // 5
+    void printNumReachableDests();
+    // 6
+    void printMaxTrip();
+    // 7
+    void printTopKAirport();
+    // 8
+    void printEssentialAirports();
+
+    // Best flight options
     // ToDo
 
 private:
